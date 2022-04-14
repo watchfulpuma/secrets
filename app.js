b2ab5f1eccf2b1ab28ce4bd601cb584b206ab465
John Fulbright <johnfulbright@Johns-MacBook-Pro.local>
2022-04-13 22:02:43 -0500

    add url

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://morning-spire-47096.herokuapp.com/auth/google/secrets",
   },
   function(accessToken, refreshToken, profile, cb) {
 
